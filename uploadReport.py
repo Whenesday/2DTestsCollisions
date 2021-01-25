bc9f707209c5a80451851844c15373aa4af36e3f
Whenesday <dvd.teo@gmail.com>
2021-01-25 14:48:34 +0800

    Updating googleDrive upload script to create folder based on date and upload reports into it

--- uploadReport.py
+++ uploadReport.py
@@ -1,21 +1,23 @@
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
+import datetime
 
 gauth = GoogleAuth()
 gauth.LocalWebserverAuth()
 gauth.SaveCredentialsFile("creds.txt")
 drive = GoogleDrive(gauth)
+now = datetime.datetime.now().strftime("%Y-%m-%d")
 
-#TODO
-# Make the folder dynamic based on date/time and upload the report to the newly-created folder in drive
-folderCreate = drive.CreateFile({'title': 'UnityTestReports', "mimeType": "application/vnd.google-apps.folder"})
+title = "UnityTestReports-"+now
+folderCreate = drive.CreateFile({'title': title, "mimeType": "application/vnd.google-apps.folder"})
 folderCreate.Upload()
 
-folder = drive.ListFile({'q': "title = 'UnityTestReports' and trashed=false"}).GetList()[0] # get the folder we just created
-indexFile = drive.CreateFile({'parents': [{'id': folder['id']}]})
-indexFile.SetContentFile('results/index.html')
-indexFile.Upload()
-
-dashboardFile = drive.CreateFile({'parents': [{'id': folder['id']}]})
-dashboardFile.SetContentFile('results/dashboard.html')
-dashboardFile.Upload()
+folders = drive.ListFile({'q': "title='" + title + "' and mimeType='application/vnd.google-apps.folder' and trashed=false"}).GetList()
+for folder in folders:
+	if folder['title'] == title:
+		indexFile = drive.CreateFile({'parents': [{'id': folder['id']}]})
+		indexFile.SetContentFile('results/index.html')
+		indexFile.Upload()
+		dashboardFile = drive.CreateFile({'parents': [{'id': folder['id']}]})
+		dashboardFile.SetContentFile('results/dashboard.html')
+		dashboardFile.Upload()
